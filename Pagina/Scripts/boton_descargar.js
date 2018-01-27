f5b4afcb5e695f75737cc2416e5648c986492dbd
Laixdy <fernandoveliz10@gmail.com>
2018-01-27 15:51:39 -0300

    Se borro la alerta de el boton descargar

--- Pagina/Scripts/boton_descargar.js
+++ Pagina/Scripts/boton_descargar.js
@@ -1,9 +1,6 @@
 var ver = navigator.appVersion;
 var os = ver.substr(21,10);
 
-alert(os);
-alert(ver);
-
 if (os == "Win64; x64") {
 	document.getElementById("bt_ds").innerHTML = "Descargar Para Windows 64-bit";
 }
